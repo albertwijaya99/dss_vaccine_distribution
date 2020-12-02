0f800c41951d5dec353424345a221cad3b08c28e
Albert <albetz99@gmail.com>
2020-12-02 20:45:59 +0700

    updated app.py

--- app.py
+++ app.py
@@ -90,5 +90,5 @@ def result():
         return render_template("result.html", result = res_nama)
 
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
